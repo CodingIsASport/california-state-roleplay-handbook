6cb41bb72596d21bd6537ff48764e9695fb75865
Wollo_Dumlap <130704484+CodingIsASport@users.noreply.github.com>
2025-12-23 21:29:37 -0600

    Add accounts object with user credentials and roles

---
+++ accounts.js
@@ -0,0 +1,6 @@
+const accounts = {
+  "mod01": { password: "mod123", role: "low" },
+  "admin01": { password: "admin123", role: "high" },
+  "manager01": { password: "manager123", role: "senior" },
+  "director01": { password: "director123", role: "exec" }
+};
